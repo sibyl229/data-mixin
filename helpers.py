2a4bef31afbc0ba41d4cd20bf6fb1c74274282c3
Sibyl G <particle.galaxy@gmail.com>
2013-08-01 18:38:22 -0700

    vstack of structured arrays for writing features

---
+++ helpers.py
@@ -0,0 +1,25 @@
+import numpy as np 
+
+class ColumnStack:
+
+    @staticmethod
+    def stack(*ndArrs):
+        '''ensure columns in these arrays are named'''
+        # keep the dtypes of the original columns
+        dtype_arr = []
+        colCount = 0
+        for arr in ndArrs:
+            dtype_arr += arr.dtype.descr # an array of (fieldName, type) tuples
+            colCount += len(arr.dtype.descr)
+
+        # initialize the array
+        shape = (ndArrs[0].shape[0],) 
+        newArr = np.zeros(shape, dtype=dtype_arr)
+
+        # populate new array based on old value
+        for arr in ndArrs:
+            for fieldName in arr.dtype.names:
+                # import pdb; pdb.set_trace()
+                newArr[fieldName] = arr[fieldName]
+
+        return newArr
