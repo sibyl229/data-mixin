001d045b87edbec90646120b7f4dc3121ecf0c81
Sibyl G <particle.galaxy@gmail.com>
2013-08-01 18:36:59 -0700

    purpose of some directories

---
+++ local_paths.py
@@ -0,0 +1,7 @@
+'''path to several useful folders'''
+import os
+
+PROJECT_ROOT = os.path.dirname(__file__)
+RAW_INPUT_PATH = os.path.join(PROJECT_ROOT, 'raw_inputs/')
+FEATURE_FILE_PATH = os.path.join(PROJECT_ROOT, 'input/features')
+NORMALIZED_FEATURE_FILE_PATH = os.path.join(PROJECT_ROOT, 'input/normalized_features')
